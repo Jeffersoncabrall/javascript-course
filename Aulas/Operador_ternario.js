659e859947f217b325776128661e01a320bbef95
Jefferson cabral silva <jeffersoncabral.dev@gmail.com>
2026-07-22 23:01:57 +0100

    ternario

---
+++ Aulas/Operador_ternario.js
@@ -0,0 +1,8 @@
+// Operador ternário é uma forma abreviada e autoavaliada do IF ELSE
+
+// Sintaxe:
+// const resultado = condicao ? resultado_verdadeiro : resultado_falso;
+
+const age = 32;
+
+const age_people = age === 32 ? alert("Tem 32") : alert("Não tem 32");
